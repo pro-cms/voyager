9b6ac13663f671084d74c47ab1d8647542ee4387
Christoph Schweppe <info@cschweppe.de>
2021-08-03 15:13:49 +0200

    Display relationship preview

--- resources/assets/components/Formfields/Relationship/Builder.vue
+++ resources/assets/components/Formfields/Relationship/Builder.vue
@@ -33,7 +33,39 @@
         </template>
     </div>
     <template v-else-if="action == 'view'">
-        
+        <div class="voyager-table">
+            <table>
+                <thead>
+                    <tr>
+                        <th class="w-2"></th>
+                        <th class="flex">
+                            <div class="flex-none self-center">
+                                {{ translate(options.display_name, true) }}
+                            </div>
+                            <div class="flex flex-grow justify-end">
+                                <button class="button" v-if="options.allow_none" disabled>
+                                    {{ __('voyager::generic.none') }}
+                                </button>
+                            </div>
+                        </th>
+                    </tr>
+                    <tr>
+                        <th class="w-2"></th>
+                        <th>
+                            <input class="input w-full" disabled :placeholder="__('voyager::bread.search_type', {type: translate(options.display_name, true)})">
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="result in dummyResults" :key="result.key">
+                        <td class="w-2">
+                            <input type="checkbox" class="input" disabled>
+                        </td>
+                        <td>{{ result.value }}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     </template>
 </template>
 
@@ -60,5 +92,16 @@ export default {
             return undefined;
         }
     },
+    data() {
+        return {
+            dummyResults: [
+                { key: 1, value: 'Lorem ipsum dolor sit amet' },
+                { key: 2, value: 'Lorem ipsum dolor sit amet' },
+                { key: 3, value: 'Lorem ipsum dolor sit amet' },
+                { key: 4, value: 'Lorem ipsum dolor sit amet' },
+                { key: 5, value: 'Lorem ipsum dolor sit amet' },
+            ]
+        };
+    }
 }
 </script>
