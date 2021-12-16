5aeb33d861a69768621742c226237cc2ccbf0be1
emptynick <emptynick@users.noreply.github.com>
2021-12-16 12:56:54 +0000

    Build documentation

---
+++ docs/.vitepress/dist/assets/contributing_assets.md.bdbb8659.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o as s,a}from"./app.44eaed71.js";const m='{"title":"Assets","description":"","frontmatter":{},"relativePath":"contributing/assets.md","lastUpdated":1639659388962}',o={},r=a("",3),i=[r];function c(l,n,d,u,p,_){return s(),t("div",null,i)}var f=e(o,[["render",c]]);export{m as __pageData,f as default};
---
+++ docs/.vitepress/dist/assets/de_index.md.042153fd.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o as a}from"./app.44eaed71.js";const f='{"title":"","description":"","frontmatter":{},"relativePath":"de/index.md","lastUpdated":1639659388962}',r={};function o(n,s,c,d,p,i){return a(),t("div")}var l=e(r,[["render",o]]);export{f as __pageData,l as default};
